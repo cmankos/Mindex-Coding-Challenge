0cc3db03ebaa312200e3014ffc88569c2bed48a8
cmankos <cmankos@gmail.com>
2024-01-07 22:10:32 -0500

    Adding tests for reporting structure counts

--- src/test/java/com/mindex/challenge/service/impl/ReportingStructureServiceImplTest.java
+++ src/test/java/com/mindex/challenge/service/impl/ReportingStructureServiceImplTest.java
@@ -1,53 +1,97 @@
 package com.mindex.challenge.service.impl;
 
+import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.service.ReportingStructureService;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import static org.mockito.Mockito.*;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 
-// Tests I'm interested in:
-// A -> B; B -> A yields 1
-// A -> B, C; B -> C yields 2
-// straightforwards like A -> B yields 1, A -> B; B -> C, D yields 3
-// Going to need to mock all of this. Core idea. Or maybe I'll just add it to separate testing db. Undecided.
+@RunWith(SpringRunner.class)
 public class ReportingStructureServiceImplTest {
 
-    @Autowired
-    ReportingStructureService reportingStructureService;
+    private ReportingStructureService reportingStructureService;
+
+    @MockBean
+    EmployeeRepository employeeRepository;
+
+    @Before
+    public void init(){
+        reportingStructureService = new ReportingStructureServiceImpl(employeeRepository);
+    }
 
     @Test
-    public void findReportingStructureTest() {
-        Employee A1 = new Employee();
-        Employee B1 = new Employee();
-        Employee A2 = new Employee();
-        Employee B2 = new Employee();
-        Employee C2 = new Employee();
-        Employee A3 = new Employee();
-        Employee B3 = new Employee();
-        Employee C3 = new Employee();
-        Employee D3 = new Employee();
+    public void whenStraightforwardTreeStructureThenCountedAsExpectedTest(){
 
-        // Shortcutting a little here
-        A1.setEmployeeId("16a596ae-edd3-4847-99fe-c4518e82c86f");
-        B1.setEmployeeId("16a596ae-edd3-4847-99fe-c4518e82c86f");
-        A1.setDirectReports(new ArrayList() {{add(B1);}});
-        B1.setDirectReports(new ArrayList() {{add(A1);}});
+        Employee employeeA = new Employee();
+        Employee employeeB = new Employee();
+        Employee employeeC = new Employee();
+        Employee employeeD = new Employee();
 
-        A2.setEmployeeId("16a596ae-edd3-4847-99fe-c4518e82c86f");
-        B2.setEmployeeId("16a596ae-edd3-4847-99fe-c4518e82c86f");
-        C2.setEmployeeId("16a596ae-edd3-4847-99fe-c4518e82c86f");
-        A2.setDirectReports(new ArrayList() {{add(B2); add(C2);}});
-        B2.setDirectReports(new ArrayList() {{add(C2);}});
+        employeeA.setEmployeeId("employeeA");
+        employeeB.setEmployeeId("employeeB");
+        employeeC.setEmployeeId("employeeC");
+        employeeD.setEmployeeId("employeeD");
+        employeeA.setDirectReports(new ArrayList() {{add(employeeB);}});
+        employeeB.setDirectReports(new ArrayList() {{add(employeeC); add(employeeD);}});
 
-        A3.setEmployeeId("16a596ae-edd3-4847-99fe-c4518e82c86f");
-        B3.setEmployeeId("16a596ae-edd3-4847-99fe-c4518e82c86f");
-        C3.setEmployeeId("16a596ae-edd3-4847-99fe-c4518e82c86f");
-        D3.setEmployeeId("16a596ae-edd3-4847-99fe-c4518e82c86f");
-        A3.setDirectReports(new ArrayList() {{add(B3);}});
-        B3.setDirectReports(new ArrayList() {{add(C3); add(D3);}});
+        when(employeeRepository.findByEmployeeId("employeeA")).thenReturn(employeeA);
+        when(employeeRepository.findByEmployeeId("employeeB")).thenReturn(employeeB);
+        when(employeeRepository.findByEmployeeId("employeeC")).thenReturn(employeeC);
+        when(employeeRepository.findByEmployeeId("employeeD")).thenReturn(employeeD);
 
-//        assert(reportingStructureService.findReportingStructure(A1.getEmployeeId()).getNumberOfReports() == 1);
+        assert(reportingStructureService.findReportingStructure(employeeA.getEmployeeId()).getNumberOfReports() == 3);
+        assert(reportingStructureService.findReportingStructure(employeeB.getEmployeeId()).getNumberOfReports() == 2);
+        assert(reportingStructureService.findReportingStructure(employeeC.getEmployeeId()).getNumberOfReports() == 0);
+        assert(reportingStructureService.findReportingStructure(employeeD.getEmployeeId()).getNumberOfReports() == 0);
+
+    }
+
+    @Test
+    public void whenMultipleAppearancesInTreeThenCountedOnceTest(){
+
+        Employee employeeA = new Employee();
+        Employee employeeB = new Employee();
+        Employee employeeC = new Employee();
+
+        employeeA.setEmployeeId("employeeA");
+        employeeB.setEmployeeId("employeeB");
+        employeeC.setEmployeeId("employeeC");
+        employeeA.setDirectReports(new ArrayList() {{add(employeeB); add(employeeC);}});
+        employeeB.setDirectReports(new ArrayList() {{add(employeeC);}});
+
+        when(employeeRepository.findByEmployeeId("employeeA")).thenReturn(employeeA);
+        when(employeeRepository.findByEmployeeId("employeeB")).thenReturn(employeeB);
+        when(employeeRepository.findByEmployeeId("employeeC")).thenReturn(employeeC);
+
+        assert(reportingStructureService.findReportingStructure(employeeA.getEmployeeId()).getNumberOfReports() == 2);
+        assert(reportingStructureService.findReportingStructure(employeeB.getEmployeeId()).getNumberOfReports() == 1);
+        assert(reportingStructureService.findReportingStructure(employeeC.getEmployeeId()).getNumberOfReports() == 0);
+    }
+
+    // Normally, I'd ask the appropriate party how they'd want to handle cases like this,
+    // but for the sake of expediency I am making some assumptions
+    @Test
+    public void whenCircularDependencyThenCountSelfTest() {
+
+        Employee employeeA = new Employee();
+        Employee employeeB = new Employee();
+
+        employeeA.setEmployeeId("employeeA");
+        employeeB.setEmployeeId("employeeB");
+        employeeA.setDirectReports(new ArrayList() {{add(employeeB);}});
+        employeeB.setDirectReports(new ArrayList() {{add(employeeA);}});
+
+        when(employeeRepository.findByEmployeeId("employeeA")).thenReturn(employeeA);
+        when(employeeRepository.findByEmployeeId("employeeB")).thenReturn(employeeB);
+
+        assert(reportingStructureService.findReportingStructure(employeeA.getEmployeeId()).getNumberOfReports() == 2);
+        assert(reportingStructureService.findReportingStructure(employeeB.getEmployeeId()).getNumberOfReports() == 2);
     }
 }
